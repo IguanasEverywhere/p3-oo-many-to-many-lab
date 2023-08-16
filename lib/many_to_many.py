3d47efd847034f9438fafa2011756193ade9378e
Scott Schwab <scottschwab86@gmail.com>
2023-08-16 11:22:37 -0500

    Complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,71 @@
 class Author:
-    pass
+
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts() if contract.author == self]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        total = 0
+        for contract in self.contracts():
+            total += contract.royalties
+
+        return total
+
 
 
 class Book:
-    pass
+
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 
 class Contract:
-    pass
+
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise Exception
+
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise Exception
+
+        if isinstance(date, str):
+            self.date = date
+        else:
+            raise Exception
+
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise Exception
+
+        Contract.all.append(self)
+
+    def contracts_by_date():
+        sorted_contracts = sorted(Contract.all, key=lambda contract: contract.date)
+        return sorted_contracts
